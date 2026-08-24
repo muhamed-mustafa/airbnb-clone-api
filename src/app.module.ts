9314d95d3a0f5308fe551881c813889ea2d8efac
Muhammed Mustafa <muuhamed14mustafa@gmail.com>
2026-08-25 00:30:54 +0300

    refactor(errors): reorganize exception filters in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { APP_FILTER } from '@nestjs/core';
+import { AuthModule } from './auth/auth.module';
 import { GlobalExceptionFilter } from './common/errors-handling/filters/global-exception-filter';
+import { HttpExceptionFilter } from './common/errors-handling/filters/http-exception.filter';
 import { ValidationExceptionFilter } from './common/errors-handling/filters/validation-exception.filter';
 import { CoreModule } from './core.module';
 import { UsersModule } from './users/users.module';
-import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [CoreModule, UsersModule, AuthModule],
@@ -12,6 +13,10 @@ import { AuthModule } from './auth/auth.module';
     {
       provide: APP_FILTER,
       useClass: ValidationExceptionFilter,
+    },
+    {
+      provide: APP_FILTER,
+      useClass: HttpExceptionFilter,
     },
     {
       provide: APP_FILTER,
