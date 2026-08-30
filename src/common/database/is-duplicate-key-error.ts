91c80567108949203350d6d6985e51a366047c72
Muhammed Mustafa <muuhamed14mustafa@gmail.com>
2026-08-30 20:35:36 +0300

    refactor(database): enhance duplicate key error handling

--- src/common/database/is-duplicate-key-error.ts
+++ src/common/database/is-duplicate-key-error.ts
@@ -4,12 +4,18 @@ interface DuplicateKeyError {
 }
 
 export const isDuplicateKeyError = (error: unknown): error is DuplicateKeyError =>
-  typeof error === 'object' && error !== null && 'code' in error && error.code === 11000;
+  typeof error === 'object' &&
+  error !== null &&
+  'code' in error &&
+  error.code === 11000 &&
+  'keyPattern' in error &&
+  typeof error.keyPattern === 'object' &&
+  error.keyPattern !== null;
 
 export const getDuplicateKeyField = (error: unknown): string | undefined => {
-  if (isDuplicateKeyError(error)) {
-    return Object.keys(error.keyPattern ?? {})[0];
+  if (!isDuplicateKeyError(error)) {
+    return undefined;
   }
 
-  return undefined;
+  return Object.keys(error.keyPattern)[0];
 };
--- src/core.module.ts
+++ src/core.module.ts
@@ -16,12 +16,10 @@ import { envSchema } from './common/config/env.schema';
 
     I18nModule.forRoot({
       fallbackLanguage: 'en',
-
       loaderOptions: {
         path: path.join(__dirname, '/i18n/'),
         watch: true,
       },
-
       resolvers: [AcceptLanguageResolver],
     }),
 
