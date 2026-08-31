d3d56b523261465e4d4ef6449de4d6453b38229a
Muhammed Mustafa <muuhamed14mustafa@gmail.com>
2026-08-31 19:39:09 +0300

    refactor(users): integrate Mongoose repository for user management

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,10 +3,12 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './schemas/user.schema';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { MongooseUsersRepository } from './repositories/mongoose-users.repository';
+import { USER_REPOSITORY_TOKEN } from './repositories/user-repository.token';
 
 @Module({
   imports: [MongooseModule.forFeature([{ name: User.name, schema: UserSchema }])],
-  providers: [UsersService],
+  providers: [UsersService, { provide: USER_REPOSITORY_TOKEN, useClass: MongooseUsersRepository }],
   controllers: [UsersController],
   exports: [UsersService],
 })
